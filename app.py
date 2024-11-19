9d9df0a2bb5e6f622c0269d9c92a05f0d0f0a479
Umang Agarwal <59832370+umangagarwal08@users.noreply.github.com>
2024-11-19 22:44:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 import streamlit as st # type: ignore
 from mainn import recommend_products_with_clustering
 from mainn import lst
-from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
-import pandas as pd
-import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.metrics.pairwise import cosine_similarity
-from scipy.sparse import hstack
+
 
 
 input = st.selectbox("Choose a Product",lst)
