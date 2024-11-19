a5b71d35c16181d5dd10e735045d49f586760aa4
Umang Agarwal <59832370+umangagarwal08@users.noreply.github.com>
2024-11-19 23:41:33 +0530

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -44,7 +44,7 @@ def recommend_products_with_clustering(product_title, num_recommendations=10):
     try:
         # Find the closest match for the product title using fuzzy matching
         #closest_match = process.extractOne(product_title, df['Product_new'])[0]
-        product_idx = df[df['Product_new'] == product_title].index[0]
+        product_idx = df[df['Product'] == product_title].index[0]
         
         # Find the cluster of the queried product
         product_cluster = df.loc[product_idx, 'Cluster']
